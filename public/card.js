1fce64b7438afbfa01b9727c486bc480b62b6082
Zane Finucane-Shook <zane@Zanes-Mac-mini.local>
2021-08-02 10:29:40 -0700

    rendering client

--- public/card.js
+++ public/card.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', async () => {
   // Load the publishable key from the server. The publishable key
   // is set in your .env file.
-  const {publishableKey} = await fetch('/config').then((r) => r.json());
-  if (!publishableKey) {
-    addMessage(
-      'No publishable key returned from the server. Please check `.env` and try again'
-    );
-    alert('Please set your Stripe publishable API ey in the .env file');
-  }
 
-  const stripe = Stripe(publishableKey, {
-    apiVersion: '2020-08-27',
-  });
+  //this block makes you get key from server documentation says its publishable therefore unneeded?
+  /*
+  // const {publishableKey} = await fetch('/config').then((r) => r.json());
+  // if (!publishableKey) {
+  //   addMessage(
+  //     'No publishable key returned from the server. Please check `.env` and try again'
+  //   );
+  //   alert('Please set your Stripe publishable API ey in the .env file');
+  // }
+  // const stripe = Stripe(publishableKey, {
+  //   apiVersion: '2020-08-27',
+  // });
+*/
+const stripe= Stripe('pk_test_51J2RhJGw4aurOeswHqLfFCWpq80yCYnAvWtR7Gvwp7INzY9hBIdpb28FmfCqML2hn9XYGpuUnq2GnqYITDFORwKT00OfbzTSfH')
 
   const elements = stripe.elements();
   const card = elements.create('card');
@@ -32,7 +36,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Make a call to the server to create a new
     // payment intent and store its client_secret.
     const {error: backendError, clientSecret} = await fetch(
-      '/create-payment-intent',
+      'https://swipe-host.herokuapp.com/create-payment-intent',
       {
         method: 'POST',
         headers: {
